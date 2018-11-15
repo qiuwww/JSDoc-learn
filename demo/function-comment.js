2bc239b6be20e6091b5b8fb1406cbf1bf6d43073
qiuweiwei <weiwei.qiu@beitu-inc.com>
2018-11-15 15:24:24 +0800

    update

--- demo/function-comment.js
+++ demo/function-comment.js
@@ -1,2 +1,11 @@
 /** @function myFunction */
 var myFunction = (pages) => {};
+/**
+ * 参数结构的注释
+ * @param {Object} option - 传入的对象
+ * @param {String} option.name - 参数name
+ * @param {Number} option.age - 参数age
+ */
+function bar({ name, age }) {
+    return { name, age }
+}
